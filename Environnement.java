14a99d0abd5dcf197ebbb12880eebaf15f981c64
Baptiste <97801100+Baptiste-B14@users.noreply.github.com>
2023-05-11 16:33:45 +0200

    Update Environnement.java
    Add the first map

--- Environnement.java
+++ Environnement.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Environnement {
+package com.example.stationski.Modele;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Environnement {
+
+    private int argent;
+    private ArrayList<Integer> listeEnv;
+    private Capacite capa;
+    private ArrayList<Tour> listeTours;
+    private Vague vague;
+
+
+    public Environnement(){
+
+       this.listeEnv = new ArrayList<Integer>(Arrays.asList(0,0,1,1,1,1,1,1,1,1     ,1,0,1,1,1,1,1,1,1,1,   1,0,0,0,1,1,1,1,1,1,    1,1,1,0,0,0,1,1,1,1,    1,1,1,1,1,1,0,0,0,0,  1,1,1,1,0,1,1,1,0,1 ,   1,1,1,1,0,0,0,1,0,1,    1,1,1,1,1,0,0,0,0,1   ));
+
+    }
+
+
+    public ArrayList<Integer> getList(){
+        return this.listeEnv;
+    }
+
 }
