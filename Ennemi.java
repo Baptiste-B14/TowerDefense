7fe68e3485dbbc3308c478073ae631dc7e475ee8
nareio <117767483+nareio@users.noreply.github.com>
2023-05-12 10:33:25 +0200

    Update Ennemi.java
    rajout methode deplacement
    modif position en property pour vue

--- Ennemi.java
+++ Ennemi.java
@@ -1,16 +1,46 @@
+package com.example.stationski.Modele;
+
+import javafx.beans.property.IntegerProperty;
+
 public abstract class Ennemi{
 
 
     private int pV;
-    private int posX;
-    private int posY;
+    private IntegerProperty posX;
+    private IntegerProperty posY;
     private int vitesse;
-    public Ennemi (int pV, int posX, int posY, int vitesse){
+    private Environnement env;
+    public Ennemi (int pV, int posX, int posY, int vitesse, Environnement env){
         this.pV= pV;
-        this.posX=posX;
-        this.posY=posY;
+        this.posX.setValue(posX);
+        this.posY.setValue(posY);
         this.vitesse=vitesse;
+        this.env = env;
 
+    }
+
+    public IntegerProperty getPosXP(){
+        return posX;
+    }
+    public IntegerProperty getPosYP(){
+        return posY;
+    }
+
+    public int getPosX() {
+        return posX.getValue();
+    }
+    public int getPosY() {
+        return posY.getValue();
+    }
+
+    public void agit(){
+        deplacement();
+    }
+
+    public void deplacement (){
+        //deplacement tres simple
+        posX.setValue(posX.getValue()+ (int)(Math.random()*3-1));
+        posY.setValue(posY.getValue()+ (int)(Math.random()*3-1));
     }
 
 }
