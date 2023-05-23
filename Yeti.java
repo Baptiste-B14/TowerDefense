a60726fc2073c71960b3581ca9dc2a7f448c13a4
Baptiste <97801100+Baptiste-B14@users.noreply.github.com>
2023-05-23 10:30:29 +0200

    Delete Yeti.java

--- Yeti.java
+++
@@ -1,8 +0,0 @@
-public class Yeti extends Ennemi{
-
-    private int ptsAttaque;
-    public Yeti(int pV, int vitesse, int posX, int posY, int ptsAttaque){
-        super(10, posX, posY, 2);
-        this.ptsAttaque=ptsAttaque;
-    }
-}
