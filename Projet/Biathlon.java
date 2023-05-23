ae326c50ea81d631fe62259d0fb9991fd7fb26aa
Baptiste <97801100+Baptiste-B14@users.noreply.github.com>
2023-05-23 11:08:28 +0200

    Delete Projet directory

--- Projet/Biathlon.java
+++
@@ -1,9 +0,0 @@
-public class Biathlon extends Tour{
-
-    public Biathlon (int posX, int posY, int pV, int ptsAttaque, int range){
-        super(posX, posY, pV, ptsAttaque, range);
-
-    }
-
-
-}
--- Projet/Bobsleigh.java
+++
@@ -1,8 +0,0 @@
-public class Bobsleigh extends Ennemi{
-
-    private int ptsAttaque;
-    public Bobsleigh(int pV,  int posX, int posY, int vitesse,int ptsAttaque){
-        super(10,posX, posY ,vitesse);
-        this.ptsAttaque=ptsAttaque;
-    }
-}
