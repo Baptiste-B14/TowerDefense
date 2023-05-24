6b51a12bf22212dd664853d52d8eeec2f0f6495e
nareio <much.lio@laposte.net>
2023-05-24 14:44:15 +0200

    ajout classes héritant de Ennemi et Tour

---
+++ src/main/java/fr/iut/montreuil/stationski/Modele/skieurBasique.java
@@ -0,0 +1,13 @@
+package fr.iut.montreuil.stationski.Modele;
+
+public class skieurBasique extends Ennemi{
+
+
+    public skieurBasique(int pv, int posX, int posY, int vitesse, Environnement env, int butin) {
+        super(pv, posX, posY, vitesse, env, butin);
+    }
+
+
+
+
+}
