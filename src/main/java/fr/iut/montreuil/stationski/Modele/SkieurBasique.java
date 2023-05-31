016f1803bb64fbb2ad7a53a45c11413d096c6061
Trupin <120423796+Matthias-iut@users.noreply.github.com>
2023-05-31 11:08:02 +0200

    Update SkieurBasique.java

--- src/main/java/fr/iut/montreuil/stationski/Modele/SkieurBasique.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/SkieurBasique.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class SkieurBasique extends Ennemi{
 
 
-    public SkieurBasique(int pv, int posX, int posY, int vitesse, Environnement env, int butin, ArrayList<Sommet> pc) {
-        super(pv, posX, posY, vitesse, env, butin, pc);
+    public SkieurBasique(int pv, int posX, int posY, int vitesse, Environnement env, int butin, Dijkstra dijkstra) {
+        super(pv, posX, posY, vitesse, env, butin, dijkstra);
     }
 
 
