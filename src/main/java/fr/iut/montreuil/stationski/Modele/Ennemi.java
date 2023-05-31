af49ca5be5ad7b5c537a4f5d487f8fb087c34f66
Trupin <120423796+Matthias-iut@users.noreply.github.com>
2023-05-31 11:11:25 +0200

    Update Ennemi.java

--- src/main/java/fr/iut/montreuil/stationski/Modele/Ennemi.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/Ennemi.java
@@ -8,43 +8,32 @@ import java.util.ArrayList;
 public class Ennemi extends Entite {
     private int vitesse;
     private int butin;
-    private ArrayList<Sommet> parcours;
-    public Ennemi (int pv, int posX, int posY, int vitesse, Environnement env, int butin, ArrayList<Sommet> parcours){
+    private Dijkstra dijkstra;
+    public Ennemi (int pv, int posX, int posY, int vitesse, Environnement env, int butin, Dijkstra dijkstra){
         super(pv, posX, posY, env);
         this.vitesse=vitesse;
         this.butin = butin;
-        this.parcours = parcours;
+        this.dijkstra = dijkstra;
     }
-
-
 
 
     public void agit(){
         deplacement();
-        System.out.println("se deplace");
+        //System.out.println("se deplace");
         this.prendDegats(1);
-        System.out.println("souffre");
+        //System.out.println("souffre");
     }
 
-    public void deplacement () {
+    public void deplacement (){
         //deplacement tres simple
-       // posX.setValue(posX.getValue() + (int) (Math.random() * 4) - 1 + vitesse);
-        //posY.setValue(posY.getValue() + (int) (Math.random() * 4) - 1 + vitesse);
-
-
-        if (this.parcours.size() > 0) {
-            this.posX.setValue(((Sommet) this.parcours.get(0)).getX());
-            this.posY.setValue(((Sommet) this.parcours.get(0)).getY());
-            this.parcours.remove(0);
-
-            for (int i = 0; i < this.vitesse - 1 && this.parcours.size() > this.vitesse; ++i) {
-                this.parcours.remove(0);
-            }
+        if (this.dijkstra.getParcours().size() > 0) {
+            this.posX.setValue(((Sommet)this.dijkstra.getParcours().get(this.dijkstra.getParcours().size()-1)).getX()*16);
+            this.posY.setValue(((Sommet)this.dijkstra.getParcours().get(this.dijkstra.getParcours().size()-1)).getY()*16);
+            this.dijkstra.getParcours().remove(this.dijkstra.getParcours().size()-1);
         }
-        //posX.setValue(posX.getValue()+ (int)(Math.random()*4)-1+ vitesse);
-        //posY.setValue(posY.getValue()+ (int)(Math.random()*4)-1+ vitesse);
-
-
+        else{
+            super.setPV(0);
+        }
     }
 
 
